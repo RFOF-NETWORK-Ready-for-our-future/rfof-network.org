4502c2c735a3cc608efbf7efa05bdd13ab9fe2af
RFOF-NETWORK <rfof236286@gmail.com>
2024-09-02 07:29:11 +0200

    Update jetton_bot.py

--- RFOF_BOT/venv/jetton_bot.py
+++ RFOF_BOT/venv/jetton_bot.py
@@ -1,1 +1,11 @@
-TOKEN = os.getenv("TELEGRAM_BOT_API_KEY")
+TOKEN = os.getenv("TELEGRAM_BOT_API_KEY") ("6774549752:AAFx5D-reu3Pu-qkmKLh2aNxumv4faZtRN0")
+
+f"Total Supply: 999999999999999999999999900\n"
+        f"Mintable: true\n"
+        f"Admin Address: 0:e4fb51aa7386080b6d8b4c00192f1a26864ca9f95e76074c9a787826937b7d2e\n"
+        f"Master Address: EQDYMcZf6OAdtvhJma887wHdyH_dowjYWoiOf9zy5Mm5-q8D\n"
+        f"Wallet Address: EQC_LhrYP8fvFgtDLEhrWnuSLo8w4cCI4o-CFSIGOCOTmwjj\n"
+        f"Holder Address: EQDYMcZf6OAdtvhJma887wHdyH_dowjYWoiOf9zy5Mm5-q8D\n"
+        f"Hey Hash: 8da55a166c83c8cea677f2fbb4925ef81eda782cee73bacaa82d5c4e87882138\n"
+        f"Jetton Content: Contains metadata of the token, such as the name and description.\n"
+        f"Jetton Wallet Code: The code of the smart contract that defines the logic for the Jetton wallet."
