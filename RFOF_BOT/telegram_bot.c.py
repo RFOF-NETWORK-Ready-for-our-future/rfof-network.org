1a4bca2bea15a646d90920f2bf7cfab98641edea
RFOF-NETWORK <rfof236286@gmail.com>
2024-09-02 07:26:34 +0200

    Update telegram_bot.c.py

--- RFOF_BOT/telegram_bot.c.py
+++ RFOF_BOT/telegram_bot.c.py
@@ -29,7 +29,15 @@ ADMIN_ADDRESS = "0:e4fb51aa7386080b6d8b4c00192f1a26864ca9f95e76074c9a787826937b7
 MASTER_ADDRESS = "EQDYMcZf6OAdtvhJma887wHdyH_dowjYWoiOf9zy5Mm5-q8D"
 JETTON_CONTENT = "Metadata about the token"
 JETTON_WALLET_CODE = "Smart contract code for the Jetton wallet"
-
+f"Total Supply: 999999999999999999999999900\n"
+        f"Mintable: true\n"
+        f"Admin Address: 0:e4fb51aa7386080b6d8b4c00192f1a26864ca9f95e76074c9a787826937b7d2e\n"
+        f"Master Address: EQDYMcZf6OAdtvhJma887wHdyH_dowjYWoiOf9zy5Mm5-q8D\n"
+        f"Wallet Address: EQC_LhrYP8fvFgtDLEhrWnuSLo8w4cCI4o-CFSIGOCOTmwjj\n"
+        f"Holder Address: EQDYMcZf6OAdtvhJma887wHdyH_dowjYWoiOf9zy5Mm5-q8D\n"
+        f"Hey Hash: 8da55a166c83c8cea677f2fbb4925ef81eda782cee73bacaa82d5c4e87882138\n"
+        f"Jetton Content: Contains metadata of the token, such as the name and description.\n"
+        f"Jetton Wallet Code: The code of the smart contract that defines the logic for the Jetton wallet."
 # ABI für das Jetton Token definieren
 abi = Abi.from_file(ABI_FILE)
 
