edb3e16a8db5ae28d699c9a5ab84e82d848498d2
rfofnetwork <rfof236286@gmail.com>
2024-10-08 02:31:40 +0200

    Update deploy.js

--- TonWhitelistProject/scripts/deploy.js
+++ TonWhitelistProject/scripts/deploy.js
@@ -1,1 +1,16 @@
+const hre = require("hardhat");
 
+async function main() {
+    const Whitelist = await hre.ethers.getContractFactory("Whitelist");
+    const whitelist = await Whitelist.deploy();
+
+    await whitelist.deployed();
+    console.log("Whitelist Contract deployed to:", whitelist.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
