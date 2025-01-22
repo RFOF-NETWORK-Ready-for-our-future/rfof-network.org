2ad50fcca79d6e206b5f366e8b9436779947b5aa
rfof-network.org <176731182+RFOF-NETWORK@users.noreply.github.com>
2025-01-22 19:49:10 +0100

    Update core.initializer.py

--- Chain’s/@RFOF-NETWORK/quantum_genesis/core.initializer.py
+++ Chain’s/@RFOF-NETWORK/quantum_genesis/core.initializer.py
@@ -35,29 +35,24 @@ class RFOFBlockchain:
         }
 
     def generate_wallet_address(self, label):
-        # Erzeugt eine eindeutige Wallet-Adresse basierend auf dem Label
         return hashlib.sha256(f"{label}_{time.time()}".encode()).hexdigest()
 
-    # #GENESIS_BLOCK
     def create_arc_genesis_reactor_block(self):
         if not self.genesis_created:
-            # Zuerst wird der Genesis Block erstellt
             self.create_genesis_block()
-            # Dann wird der ARC Genesis Reaktor Block erstellt und die Fusion initiiert
             arc_reactor_block = {
                 "index": 1,
                 "timestamp": time.time(),
                 "data": "ARC Genesis Reaktor Block",
                 "previous_hash": self.blocks[-1]["hash"],
                 "hash": self.hash_block("ARC Genesis Reaktor Block"),
-                "extra_features": self.initialize_arc_features()  # Eigene Funktionen des RFOF-Netzwerks
+                "extra_features": self.initialize_arc_features()
             }
             self.blocks.append(arc_reactor_block)
             self.genesis_created = True
             print("ARC Genesis Reaktor Block created with extra features.")
             self.fusion_genesis_arc()
 
-    # #GENESIS_BLOCK
     def create_genesis_block(self):
         genesis_block = {
             "index": 0,
@@ -65,12 +60,11 @@ class RFOFBlockchain:
             "data": "Genesis Block",
             "previous_hash": "0",
             "hash": self.hash_block("Genesis Block"),
-            "extra_features": self.initialize_genesis_features()  # Funktionen des Bitcoin-Netzwerks
+            "extra_features": self.initialize_genesis_features()
         }
         self.blocks.append(genesis_block)
         print("Genesis Block created.")
 
-    # #FEATURES
     def initialize_genesis_features(self):
         features = {
             "Bitcoin_network": True,
@@ -78,7 +72,6 @@ class RFOFBlockchain:
         }
         return features
 
-    # #FEATURES
     def initialize_arc_features(self):
         features = {
             "PRAI_guardian": True,
@@ -89,7 +82,6 @@ class RFOFBlockchain:
         }
         return features
 
-    # #FUSION
     def fusion_genesis_arc(self):
         fusion_block = {
             "index": 2,
@@ -108,11 +100,10 @@ class RFOFBlockchain:
         }
         self.blocks.append(fusion_block)
         print("ARC Genesis Reactor Fusion Block created.")
-    # #HASH
+    
     def hash_block(self, data):
-        return str(hash(data))
+        return hashlib.sha256(data.encode()).hexdigest()
 
-    # #BLOCK_CREATION
     def create_block(self, transaction, from_address):
         previous_block = self.blocks[-1]
         block = self.generate_nanoblock(transaction, previous_block["hash"])
@@ -120,7 +111,6 @@ class RFOFBlockchain:
         self.handle_transaction_fee(transaction, from_address)
         return block
 
-    # #BLOCK_CREATION
     def generate_nanoblock(self, transaction, previous_hash):
         nano_block = {
             "index": len(self.blocks),
@@ -133,44 +123,36 @@ class RFOFBlockchain:
         }
         return nano_block
 
-    # #DATA_PROCESSING
     def process_data(self, transaction):
         nano_data = {"compressed_data": str(transaction)[:10]}
         return nano_data
 
-    # #VIRTUAL_BLOCKS
     def generate_virtual_blocks(self):
         virtual_blocks = []
-        for i in range(1, 100000000001):
+        for i in range(1, 1000000001):  # Reduzierte Anzahl für Beispielzwecke
             virtual_block = {"block_id": i, "data": f"virtual_block_{i}"}
             virtual_blocks.append(virtual_block)
             if i % 100 == 0:
                 self.create_validator(virtual_block)
         return virtual_blocks
 
-    # #VALIDATORS
     def create_validator(self, virtual_block):
         validator = {"validator_id": len(self.validators) + 1, "virtual_block": virtual_block}
         self.validators.append(validator)
         self.create_block(f"Validator Transaction from Block {virtual_block['block_id']}", self.coin_master_address)
         print(f"Validator {validator['validator_id']} created from virtual block {virtual_block['block_id']}")
 
-    # #TRANSACTION_FEES
     def handle_transaction_fee(self, transaction, from_address):
         fee = self.calculate_fee(transaction)
         self.store_fee_in_vault(fee, from_address)
         print(f"Transaction fee of {fee} units stored in vault from {from_address}")
 
-    # #TRANSACTION_FEES
     def calculate_fee(self, transaction):
         return 0.01 * len(transaction)
-
-    # #VAULT
     def store_fee_in_vault(self, fee, from_address):
         timestamp = time.time()
         self.vault[timestamp] = {"fee": fee, "from_address": from_address}
 
-    # #VAULT
     def process_vault(self):
         current_time = time.time()
         for timestamp, fee_info in list(self.vault.items()):
@@ -180,7 +162,6 @@ class RFOFBlockchain:
                 self.release_fee_from_vault(fee, from_address)
                 del self.vault[timestamp]
 
-    # #VAULT
     def release_fee_from_vault(self, fee, from_address):
         if from_address == "External":
             self.storage_mapper[self.coin_master_address] = self.storage_mapper.get(self.coin_master_address, 0) + fee
@@ -192,14 +173,28 @@ class PRAI:
     def __init__(self):
         self.commands = []
 
-    # #COMMANDS
     def receive_command(self, command):
         self.commands.append(command)
         return self.execute_command(command)
 
-    # #COMMANDS
     def execute_command(self, command):
         if command == "create_block":
             blockchain = RFOFBlockchain()
             transaction = "Sample Transaction Data"
-            from_address
+            from_address = blockchain.coin_master_address
+            return blockchain.create_block(transaction, from_address)
+        elif command == "create_arc_genesis_reactor_block":
+            blockchain = RFOFBlockchain()
+            return blockchain.create_arc_genesis_reactor_block()
+        # Weitere Befehle können hier hinzugefügt werden
+        else:
+            return f"Command {command} not recognized."
+
+# Beispiel für die Verwendung
+if __name__ == "__main__":
+    blockchain = RFOFBlockchain()
+    blockchain.create_arc_genesis_reactor_block()
+
+    prai = PRAI()
+    print(prai.receive_command("create_block"))
+    print(prai.receive_command("create_arc_genesis_reactor_block"))
